c4a4afa491c8125b536dd0d0f0f895ae193d8006
vissarionas <vissarionas@gmail.com>
2017-03-30 15:48:31 +0300

    jquery version

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
 var output = document.getElementById("output");
 var input = document.getElementById("input_nums");
 var numBtns = document.getElementsByClassName("numBtn");
-
 var currentValue , arg1, arg2;
-var action = "none";
-
-var plusBtn = document.getElementById("plus");
-var minusBtn = document.getElementById("minus");
-var multiplyBtn = document.getElementById("multiply");
-var divisionBtn = document.getElementById("division");
-var delBtn = document.getElementById("delete");
-var clearBtn = document.getElementById("clear");
-var equalsBtn = document.getElementById("equals");
-
+var currentAction = "none";
 
 for (var i = 0 ; i < numBtns.length ; i++) {
 	numBtns[i].addEventListener("click", bindClick(numBtns[i].value) );
 }
-
 
 function bindClick(i) {
   return function(){
@@ -27,49 +16,6 @@ function bindClick(i) {
   };
 }
 
-
-function setValues(){
-	if (!arg1){
-		arg1 = currentValue;
-	} else {
-		arg2 = currentValue;
-		if (input.value){
-			doTheMath(arg1 , arg2);
-		}
-	}
-}
-
-function doTheMath(arg_1 , arg_2){
-	switch (action){
-		case "plus":
-			output.innerHTML = parseFloat(arg1)+parseFloat(arg2);
-			arg1 = parseFloat(arg1)+parseFloat(arg2);
-			break;
-		case "minus":
-			output.innerHTML = parseFloat(arg1)-parseFloat(arg2);
-			arg1 = parseFloat(arg1)-parseFloat(arg2);
-			break;
-		case "multiply":
-			output.innerHTML = parseFloat(arg1)*parseFloat(arg2);
-			arg1 = parseFloat(arg1)*parseFloat(arg2);
-			break;
-		case "division":
-			output.innerHTML = parseFloat(arg1)/parseFloat(arg2);
-			arg1 = parseFloat(arg1)/parseFloat(arg2);
-			break;
-	}
-}
-
-
-input.addEventListener('input' , function(){ currentValue = input.value; output.innerHTML = input.value});
-plusBtn.addEventListener("click" , function(){ setValues(); action = "plus"; input.value = ""; });
-minusBtn.addEventListener("click" , function(){ setValue(s); action = "minus"; input.value = "";	});
-multiplyBtn.addEventListener("click" , function(){ setValues(); action = "multiply"; input.value = ""; });
-divisionBtn.addEventListener("click" , function(){ setValues(); action = "division"; input.value = ""; });
-equalsBtn.addEventListener("click" , function(){ setValues(); action = "equals"; input.value = ""; currentValue = "";});
-delBtn.addEventListener("click" , trimCurrentValue);
-
-
 function trimCurrentValue(){
 	if (currentValue){
 		currentValue = currentValue.substring(0, currentValue.length-1);
@@ -78,11 +24,70 @@ function trimCurrentValue(){
 	}
 }
 
-function clear(){
-	input.value = "";
-	output.innerHTML = ".";
-	arg1 = "";
-	arg2 = "";
+
+function setValue(action){
+	return function(){
+		if (!arg1){
+			arg1 = currentValue;
+		} else {
+			arg2 = currentValue;
+			if (input.value){
+				doTheMath(arg1 , arg2);
+			}
+		}
+		if(action != "equals"){
+			currentAction = action;
+		}
+		input_nums.value = "";
+	}
 }
 
-clearBtn.addEventListener("click" , clear);
+function doTheMath(arg_1 , arg_2){
+	switch (currentAction){
+		case "plus":
+			output.innerHTML = parseFloat(arg_1)+parseFloat(arg_2);
+			arg1 = parseFloat(arg_1)+parseFloat(arg_2);
+			break;
+		case "minus":
+			output.innerHTML = parseFloat(arg_1)-parseFloat(arg_2);
+			arg1 = parseFloat(arg_1)-parseFloat(arg_2);
+			break;
+		case "multiply":
+			output.innerHTML = parseFloat(arg_1)*parseFloat(arg_2);
+			arg1 = parseFloat(arg_1)*parseFloat(arg_2);
+			break;
+		case "division":
+			output.innerHTML = parseFloat(arg_1)/parseFloat(arg_2);
+			arg1 = parseFloat(arg_1)/parseFloat(arg_2);
+			break;
+		case "equals":
+			input.value = "";
+			currentValue = "";
+			break;
+	}
+}
+
+$("#input_nums").keyup(function(){
+	currentValue = input.value;
+	output.innerHTML = input.value;
+});
+
+$("#plus").click(setValue("plus"));
+
+$("#minus").click(setValue("minus"));
+
+$("#multiply").click(setValue("multiply"));
+
+$("#division").click(setValue("division"));
+
+$("#equals").click(setValue("equals"));
+
+$("#clear").click(function(){
+  input.value = "";
+	arg1 = "";
+	arg2 = "";
+	currentValue = "";
+  output.innerHTML = ".";
+});
+
+$("#delete").click(trimCurrentValue);
